d775712b64e1ffdeee92cca30c4684d9360e0baf
moonimooni <munhw1408@gmail.com>
2021-04-28 18:12:13 +0900

    Add : 모델링 수정(Media와 Theme 관계 설정 & 경매 위치 테이블 추가)

--- bids/models.py
+++ bids/models.py
@@ -3,14 +3,24 @@ from django.db import models
 class Auction(models.Model):
     start_date  = models.DateTimeField()
     end_date    = models.DateTimeField()
-    location    = models.CharField(max_length=100)
+    location    = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True)
     subscriber  = models.ManyToManyField('users.User', through='AuctionUser')
-    latitude_x  = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
-    longitude_y = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
     art         = models.ManyToManyField('arts.Art', through='AuctionArt')
 
     class Meta:
         db_table = 'auctions'
+
+class Location(models.Model):
+    name           = models.CharField(max_length=100)
+    address        = models.CharField(max_length=200)
+    phone_number   = models.CharField(max_length=20)
+    site_url       = models.URLField(max_length=2000, null=True, blank=True)
+    operating_hour = models.CharField(max_length=45, null=True, blank=True)
+    latitude_x     = models.DecimalField(max_digits=20, decimal_places=16)
+    longitude_y    = models.DecimalField(max_digits=20, decimal_places=16)
+
+    class Meta:
+        db_table = 'locations'
 
 class AuctionArt(models.Model):
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
