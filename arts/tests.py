c2c5105e1698b780c2659beee0b2e7f56c3b0016
moonimooni <munhw1408@gmail.com>
2021-04-29 17:21:33 +0900

    Add : GET BannerView

--- arts/tests.py
+++ arts/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from .models import Banner
+
+class BannerTest(TestCase):
+    def setUp(self):
+        client = Client()
+
+        Banner.objects.create(
+            id        = 2,
+            image_url = 'https://bbbb.jpeg',
+            link_url  = '#',
+            text      = '블라'
+        )
+
+        Banner.objects.create(
+            id        = 3,
+            image_url = 'https://aaaa.jpeg',
+            link_url  = '#',
+            text      = '블'
+        )
+
+    def tearDown(self):
+        Banner.objects.all().delete()
+
+    def test_banners_get_success(self):
+        client   = Client()
+        response = client.get('/banners')
+        self.assertEqual(response.json()['banners'][-1],
+            {
+                'link_url'  : '#',
+                'image_url' : 'https://aaaa.jpeg',
+                'text'      : '블'
+            }
+        )
+        self.assertEqual(response.status_code, 200)
